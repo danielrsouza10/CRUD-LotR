6b22208cbd1500639f998ad0c546b9ad6bcb33e9
Daniel Souza <danielrsouza10@gmail.com>
2024-05-14 14:11:23 -0300

    adicionado arquito teste base, instalado pacote nuget e criado propriedade ServiceProvider

---
+++ Testes/TesteBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    public class TesteBase : IDisposable
+    {
+        protected ServiceProvider _serviceProvider;
+
+        public TesteBase(ServiceProvider ServiceProvider)
+        {
+            _serviceProvider = ServiceProvider;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
