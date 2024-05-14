28b7e7614f01e2da2ec04c5f51a1d042f0ee6bef
Daniel Souza <danielrsouza10@gmail.com>
2024-05-14 10:06:40 -0300

    Criadas as classes Personagem e Raça

--- Dominio/Class1.cs
+++ Dominio/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Dominio
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Dominio
+{
+    internal class Class1
+    {
     }
 }
---
+++ Dominio/Raca.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio
+{
+    public class Raca
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string LocalizacaoGeografica { get; set; }
+        public string HabilidadeRacial { get; set; }
+    }
+}
+
