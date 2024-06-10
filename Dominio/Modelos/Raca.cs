251a8cf1d61e4d11aab0371eb6dfb415c9c7385f
Daniel Souza <danielrsouza10@gmail.com>
2024-06-10 11:59:30 -0300

    adicionados os DataAnnotations nas entidades

--- Dominio/Modelos/Raca.cs
+++ Dominio/Modelos/Raca.cs
@@ -6,9 +6,13 @@ namespace Dominio.Modelos
     [Table("Raca")]
     public class Raca
     {
+        [PrimaryKey, Identity]
         public int Id { get; set; }
+        [Column("NomeRaca"), NotNull]
         public string Nome { get; set; }
+        [Column]
         public string? LocalizacaoGeografica { get; set; }
+        [Column]
         public string? HabilidadeRacial { get; set; }
     }
 }
