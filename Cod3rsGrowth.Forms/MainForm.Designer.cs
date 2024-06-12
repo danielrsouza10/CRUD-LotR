1da402ae28b19d8fb19695a4f2aed17e61048dfa
Daniel Souza <danielrsouza10@gmail.com>
2024-06-12 13:54:48 -0300

    adicionado gridview e botões adicionar, editar, remover

--- Cod3rsGrowth.Forms/MainForm.Designer.cs
+++ Cod3rsGrowth.Forms/MainForm.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            personagemBindingSource = new BindingSource(components);
+            idDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nomeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            idRacaDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            profissaoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            idadeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            alturaDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            estaVivoDataGridViewCheckBoxColumn = new DataGridViewCheckBoxColumn();
+            dataDoCadastroDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { idDataGridViewTextBoxColumn, nomeDataGridViewTextBoxColumn, idRacaDataGridViewTextBoxColumn, profissaoDataGridViewTextBoxColumn, idadeDataGridViewTextBoxColumn, alturaDataGridViewTextBoxColumn, estaVivoDataGridViewCheckBoxColumn, dataDoCadastroDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = personagemBindingSource;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(791, 400);
+            dataGridView1.TabIndex = 0;
+            // 
+            // personagemBindingSource
+            // 
+            personagemBindingSource.DataSource = typeof(Dominio.Modelos.Personagem);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            idDataGridViewTextBoxColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            idDataGridViewTextBoxColumn.HeaderText = "Id";
+            idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            // 
+            // idRacaDataGridViewTextBoxColumn
+            // 
+            idRacaDataGridViewTextBoxColumn.DataPropertyName = "IdRaca";
+            idRacaDataGridViewTextBoxColumn.HeaderText = "IdRaca";
+            idRacaDataGridViewTextBoxColumn.Name = "idRacaDataGridViewTextBoxColumn";
+            // 
+            // profissaoDataGridViewTextBoxColumn
+            // 
+            profissaoDataGridViewTextBoxColumn.DataPropertyName = "Profissao";
+            profissaoDataGridViewTextBoxColumn.HeaderText = "Profissao";
+            profissaoDataGridViewTextBoxColumn.Name = "profissaoDataGridViewTextBoxColumn";
+            // 
+            // idadeDataGridViewTextBoxColumn
+            // 
+            idadeDataGridViewTextBoxColumn.DataPropertyName = "Idade";
+            idadeDataGridViewTextBoxColumn.HeaderText = "Idade";
+            idadeDataGridViewTextBoxColumn.Name = "idadeDataGridViewTextBoxColumn";
+            // 
+            // alturaDataGridViewTextBoxColumn
+            // 
+            alturaDataGridViewTextBoxColumn.DataPropertyName = "Altura";
+            alturaDataGridViewTextBoxColumn.HeaderText = "Altura";
+            alturaDataGridViewTextBoxColumn.Name = "alturaDataGridViewTextBoxColumn";
+            // 
+            // estaVivoDataGridViewCheckBoxColumn
+            // 
+            estaVivoDataGridViewCheckBoxColumn.DataPropertyName = "EstaVivo";
+            estaVivoDataGridViewCheckBoxColumn.HeaderText = "EstaVivo";
+            estaVivoDataGridViewCheckBoxColumn.Name = "estaVivoDataGridViewCheckBoxColumn";
+            // 
+            // dataDoCadastroDataGridViewTextBoxColumn
+            // 
+            dataDoCadastroDataGridViewTextBoxColumn.DataPropertyName = "DataDoCadastro";
+            dataDoCadastroDataGridViewTextBoxColumn.HeaderText = "DataDoCadastro";
+            dataDoCadastroDataGridViewTextBoxColumn.Name = "dataDoCadastroDataGridViewTextBoxColumn";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(566, 418);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Adicionar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(647, 418);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Editar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(728, 418);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Remover";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(815, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn idRacaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn profissaoDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn idadeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn alturaDataGridViewTextBoxColumn;
+        private DataGridViewCheckBoxColumn estaVivoDataGridViewCheckBoxColumn;
+        private DataGridViewTextBoxColumn dataDoCadastroDataGridViewTextBoxColumn;
+        private BindingSource personagemBindingSource;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
