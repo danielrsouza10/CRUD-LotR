7c1907902a1943e2aa17241516b7d5d3c8209376
Daniel Souza <danielrsouza10@gmail.com>
2024-06-13 14:03:54 -0300

    adicionado eventos para buscar e filtragem da lista

--- Cod3rsGrowth.Forms/MainForm.Designer.cs
+++ Cod3rsGrowth.Forms/MainForm.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
             dataGridView1 = new DataGridView();
             idDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
             nomeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
@@ -36,18 +45,23 @@
             profissaoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
             idadeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
             alturaDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            estaVivoDataGridViewCheckBoxColumn = new DataGridViewCheckBoxColumn();
+            estaVivoDataGridViewCheckBoxColumn = new DataGridViewTextBoxColumn();
             dataDoCadastroDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            personagemBindingSource = new BindingSource(components);
             personagemBindingSource2 = new BindingSource(components);
             personagemBindingSource1 = new BindingSource(components);
-            personagemBindingSource = new BindingSource(components);
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            Id = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).BeginInit();
             ((System.ComponentModel.ISupportInitialize)personagemBindingSource2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)personagemBindingSource1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
@@ -56,61 +70,93 @@
             dataGridView1.BackgroundColor = SystemColors.Control;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { idDataGridViewTextBoxColumn, nomeDataGridViewTextBoxColumn, idRacaDataGridViewTextBoxColumn, profissaoDataGridViewTextBoxColumn, idadeDataGridViewTextBoxColumn, alturaDataGridViewTextBoxColumn, estaVivoDataGridViewCheckBoxColumn, dataDoCadastroDataGridViewTextBoxColumn });
-            dataGridView1.DataSource = personagemBindingSource2;
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.DataSource = personagemBindingSource;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = SystemColors.Window;
+            dataGridViewCellStyle9.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle9.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle9;
+            dataGridView1.Location = new Point(12, 40);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(791, 400);
+            dataGridView1.Size = new Size(791, 372);
             dataGridView1.TabIndex = 0;
             // 
             // idDataGridViewTextBoxColumn
             // 
             idDataGridViewTextBoxColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle1;
             idDataGridViewTextBoxColumn.HeaderText = "Id";
             idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
             // 
             // nomeDataGridViewTextBoxColumn
             // 
             nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            nomeDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle2;
             nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
             nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
             // 
             // idRacaDataGridViewTextBoxColumn
             // 
             idRacaDataGridViewTextBoxColumn.DataPropertyName = "IdRaca";
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idRacaDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle3;
             idRacaDataGridViewTextBoxColumn.HeaderText = "IdRaca";
             idRacaDataGridViewTextBoxColumn.Name = "idRacaDataGridViewTextBoxColumn";
             // 
             // profissaoDataGridViewTextBoxColumn
             // 
             profissaoDataGridViewTextBoxColumn.DataPropertyName = "Profissao";
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            profissaoDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle4;
             profissaoDataGridViewTextBoxColumn.HeaderText = "Profissao";
             profissaoDataGridViewTextBoxColumn.Name = "profissaoDataGridViewTextBoxColumn";
             // 
             // idadeDataGridViewTextBoxColumn
             // 
             idadeDataGridViewTextBoxColumn.DataPropertyName = "Idade";
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idadeDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle5;
             idadeDataGridViewTextBoxColumn.HeaderText = "Idade";
             idadeDataGridViewTextBoxColumn.Name = "idadeDataGridViewTextBoxColumn";
             // 
             // alturaDataGridViewTextBoxColumn
             // 
             alturaDataGridViewTextBoxColumn.DataPropertyName = "Altura";
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            alturaDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle6;
             alturaDataGridViewTextBoxColumn.HeaderText = "Altura";
             alturaDataGridViewTextBoxColumn.Name = "alturaDataGridViewTextBoxColumn";
             // 
             // estaVivoDataGridViewCheckBoxColumn
             // 
             estaVivoDataGridViewCheckBoxColumn.DataPropertyName = "EstaVivo";
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            estaVivoDataGridViewCheckBoxColumn.DefaultCellStyle = dataGridViewCellStyle7;
             estaVivoDataGridViewCheckBoxColumn.HeaderText = "EstaVivo";
             estaVivoDataGridViewCheckBoxColumn.Name = "estaVivoDataGridViewCheckBoxColumn";
+            estaVivoDataGridViewCheckBoxColumn.Resizable = DataGridViewTriState.True;
+            estaVivoDataGridViewCheckBoxColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
             // 
             // dataDoCadastroDataGridViewTextBoxColumn
             // 
             dataDoCadastroDataGridViewTextBoxColumn.DataPropertyName = "DataDoCadastro";
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.Format = "d";
+            dataGridViewCellStyle8.NullValue = null;
+            dataDoCadastroDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle8;
             dataDoCadastroDataGridViewTextBoxColumn.HeaderText = "DataDoCadastro";
             dataDoCadastroDataGridViewTextBoxColumn.Name = "dataDoCadastroDataGridViewTextBoxColumn";
+            // 
+            // personagemBindingSource
+            // 
+            personagemBindingSource.DataSource = typeof(Dominio.Modelos.Personagem);
             // 
             // personagemBindingSource2
             // 
@@ -119,10 +165,6 @@
             // personagemBindingSource1
             // 
             personagemBindingSource1.DataSource = typeof(Dominio.Modelos.Personagem);
-            // 
-            // personagemBindingSource
-            // 
-            personagemBindingSource.DataSource = typeof(Dominio.Modelos.Personagem);
             // 
             // button1
             // 
@@ -151,41 +193,103 @@
             button3.Text = "Remover";
             button3.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 10);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(485, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Buscar personagem...";
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.Enter += textBox1_Enter;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(503, 12);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(59, 19);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Nome";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(610, 12);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(70, 19);
+            checkBox2.TabIndex = 7;
+            checkBox2.Text = "EstaVivo";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(686, 12);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(117, 19);
+            checkBox3.TabIndex = 8;
+            checkBox3.Text = "Data do Cadastro";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Id
+            // 
+            Id.AutoSize = true;
+            Id.Location = new Point(568, 12);
+            Id.Name = "Id";
+            Id.Size = new Size(36, 19);
+            Id.TabIndex = 9;
+            Id.Text = "Id";
+            Id.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(815, 450);
+            Controls.Add(Id);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "MainForm";
-            Text = "Form1";
+            Text = "O Senhor dos Anéis DB";
             Load += MainForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).EndInit();
             ((System.ComponentModel.ISupportInitialize)personagemBindingSource2).EndInit();
             ((System.ComponentModel.ISupportInitialize)personagemBindingSource1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)personagemBindingSource).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn idRacaDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn profissaoDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn idadeDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn alturaDataGridViewTextBoxColumn;
-        private DataGridViewCheckBoxColumn estaVivoDataGridViewCheckBoxColumn;
-        private DataGridViewTextBoxColumn dataDoCadastroDataGridViewTextBoxColumn;
         private BindingSource personagemBindingSource;
         private Button button1;
         private Button button2;
         private Button button3;
         private BindingSource personagemBindingSource1;
         private BindingSource personagemBindingSource2;
+        private TextBox textBox1;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn idRacaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn profissaoDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn idadeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn alturaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn estaVivoDataGridViewCheckBoxColumn;
+        private DataGridViewTextBoxColumn dataDoCadastroDataGridViewTextBoxColumn;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox Id;
     }
 }
--- Cod3rsGrowth.Forms/MainForm.cs
+++ Cod3rsGrowth.Forms/MainForm.cs
@@ -11,33 +11,34 @@ namespace Forms
     {
         private readonly ServicoPersonagem _servicoPersonagem;
 
-      
+
         public MainForm(ServicoPersonagem servicoPersonagem)
         {
             _servicoPersonagem = servicoPersonagem;
             InitializeComponent();
         }
 
-        Filtro filtro = new Filtro();
+
         private void MainForm_Load(object sender, EventArgs e)
         {
             InicializarListaDePersonagens();
+        }
+        private void InicializarListaDePersonagens()
+        {
+            Filtro filtro = new Filtro();
             dataGridView1.DataSource = _servicoPersonagem.ObterTodos(filtro);
         }
 
-        BindingList<Personagem> listaDePersonagens = new ();
- 
-        
-        private void InicializarListaDePersonagens()
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            listaDePersonagens = new BindingList<Personagem>();
+            Filtro filtro = new Filtro();
+            filtro.Nome = textBox1.Text;
+            dataGridView1.DataSource = _servicoPersonagem.ObterTodos(filtro);
+        }
 
-            listaDePersonagens.AllowNew = true;
-
-            listaDePersonagens.RaiseListChangedEvents = true;
-
-           
-            //listaDePersonagens.Add(new Personagem() { Nome = "Legolas", Profissao = ProfissaoEnum.Arqueiro });
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
         }
     }
 }
