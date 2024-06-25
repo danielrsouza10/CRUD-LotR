5d521e0d7fc8af4f95fcbfbfb9babdfa4f1f66d2
Daniel Souza <danielrsouza10@gmail.com>
2024-06-25 09:29:35 -0300

    reaproveitamento de codigo de injecao

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -8,6 +8,7 @@ using LinqToDB;
 using LinqToDB.AspNet;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Servico;
 using Servico.Servicos;
 using Testes.Interfaces;
 
@@ -47,26 +48,8 @@ namespace Forms
             return Host.CreateDefaultBuilder()
                 .ConfigureServices((services) =>
                 {
-                    services.AddScoped<PersonagemValidacao>();
-                    services.AddScoped<RacaValidacao>();
-                    services.AddScoped<ServicoPersonagem>();
-                    services.AddScoped<ServicoRaca>();
-                    services.AddScoped<DbOSenhorDosAneis>();
-                    services.AddScoped<IRepositorio<Personagem>, RepositorioPersonagem>();
-                    services.AddScoped<IRepositorio<Raca>, RepositorioRaca>();
-
-                    var connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING");
-                    if (string.IsNullOrEmpty(connectionString))
-                    {
-                        throw new InvalidOperationException("A variavel de ambiente SQLSERVER_CONNECTION_STRING não está definida");
-                    }
-                    services.AddLinqToDBContext<DbOSenhorDosAneis>((provider, options) => options.UseSqlServer(connectionString));
-                    services.AddFluentMigratorCore()
-                        .ConfigureRunner(rb => rb
-                            .AddSqlServer().WithGlobalConnectionString(connectionString)
-                            .ScanIn(typeof(AddPersonagemTable).Assembly).For.Migrations())
-                        .AddLogging(lb => lb.AddFluentMigratorConsole())
-                        .BuildServiceProvider(false);
+                   ModuloDeInjecaoInfra.BindServices(services);
+                   ModuloDeInjecaoServico.BindServices(services);
                 });
         }
         private static void UpdateDatabase(IServiceProvider serviceProvider)
