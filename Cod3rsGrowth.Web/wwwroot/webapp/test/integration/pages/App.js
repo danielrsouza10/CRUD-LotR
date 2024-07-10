55c719acba174666b11b000981fee55cb475f485
Daniel Souza <danielrsouza10@gmail.com>
2024-07-10 11:21:08 -0300

    corrigido teste do message toast

--- Cod3rsGrowth.Web/wwwroot/webapp/test/integration/pages/App.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/test/integration/pages/App.js
@@ -7,9 +7,34 @@ sap.ui.define(
 
     Opa5.createPageObjects({
       onTheAppPage: {
-        actions: {},
+        actions: {
+          iPressSearchButton() {
+            return this.waitFor({
+              id: "searchButton",
+              viewName: sViewName,
+              actions: new Press(),
+              errorMessage: "Did not find the 'Search' button on the App view",
+            });
+          },
+        },
 
-        assertions: {},
+        assertions: {
+          iShouldSeeTheSearchMessageToast() {
+            return this.waitFor({
+              // Turn off autoWait
+              autoWait: false,
+              check: function () {
+                // Locate the message toast using its class name in a jQuery function
+                return Opa5.getJQuery()(".sapMMessageToast").length > 0;
+              },
+              success() {
+                // we set the view busy, so we need to query the parent of the app
+                Opa5.assert.ok(true, "The MessageToast was shown");
+              },
+              errorMessage: "Did not find the MessageToast control",
+            });
+          },
+        },
       },
     });
   }
