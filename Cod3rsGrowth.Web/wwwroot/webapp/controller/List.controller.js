69eca0087f51fb2f2552fbb70d0863f8d8e5bc6e
Daniel Souza <danielrsouza10@gmail.com>
2024-07-11 12:02:27 -0300

    .

---
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/List.controller.js
@@ -0,0 +1,6 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+  return Controller.extend("ui5.o_senhor_dos_aneis.controller.List", {
+    onInit: function () {},
+  });
+});
