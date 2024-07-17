13fe046556e3f55d852983274ae3fac76e3ca568
Daniel Souza <danielrsouza10@gmail.com>
2024-07-17 14:01:37 -0300

    tudo pra fazer funcionar a descricao do enum na pagina (ou quase tudo)

--- Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
@@ -2,47 +2,6 @@ sap.ui.define([], () => {
   "use strict";
 
   return {
-    descricaoDaProfissao(valorProfissao) {
-      const pacoteDeRecursos = this.getOwnerComponent()
-        .getModel("i18n")
-        .getResourceBundle();
-      switch (valorProfissao) {
-        case 0:
-          return pacoteDeRecursos.getText("profissao0");
-        case 1:
-          return pacoteDeRecursos.getText("profissao1");
-        case 2:
-          return pacoteDeRecursos.getText("profissao2");
-        case 3:
-          return pacoteDeRecursos.getText("profissao3");
-        case 4:
-          return pacoteDeRecursos.getText("profissao4");
-        case 5:
-          return pacoteDeRecursos.getText("profissao5");
-        case 6:
-          return pacoteDeRecursos.getText("profissao6");
-        case 7:
-          return pacoteDeRecursos.getText("profissao7");
-        case 8:
-          return pacoteDeRecursos.getText("profissao8");
-        case 9:
-          return pacoteDeRecursos.getText("profissao9");
-        case 10:
-          return pacoteDeRecursos.getText("profissao10");
-        case 11:
-          return pacoteDeRecursos.getText("profissao11");
-        case 12:
-          return pacoteDeRecursos.getText("profissao12");
-        case 13:
-          return pacoteDeRecursos.getText("profissao13");
-        case 14:
-          return pacoteDeRecursos.getText("profissao14");
-        case 15:
-          return pacoteDeRecursos.getText("profissao15");
-        default:
-          return valorProfissao;
-      }
-    },
     formatarBooleanVivoMorto(valorVivoOuMorto) {
       const pacoteDeRecursos = this.getOwnerComponent()
         .getModel("i18n")
@@ -53,6 +12,25 @@ sap.ui.define([], () => {
         case false:
           return pacoteDeRecursos.getText("false");
       }
+    },
+    formatarCorVivoMorto(valorVivoOuMorto) {
+      const pacoteDeRecursos = this.getOwnerComponent()
+        .getModel("i18n")
+        .getResourceBundle();
+      switch (valorVivoOuMorto) {
+        case true:
+          return pacoteDeRecursos.getText("trueColor");
+        case false:
+          return pacoteDeRecursos.getText("falseColor");
+      }
+    },
+    formatarDataDeCadastro(valorDaData) {
+      let dataFormatada = new Date(valorDaData);
+      dataFormatada.toLocaleDateString("pt-BR");
+      let dia = dataFormatada.getDate().toString().padStart(2, "0");
+      let mes = (dataFormatada.getMonth() + 1).toString().padStart(2, "0"); //+1 pois no getMonth Janeiro começa com zero.
+      let ano = dataFormatada.getFullYear();
+      return dia + "/" + mes + "/" + ano;
     },
   };
 });
