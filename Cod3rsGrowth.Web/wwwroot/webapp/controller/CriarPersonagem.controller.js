80adf051b96a48c74225c98b3bacd56330fe00c8
Daniel Souza <danielrsouza10@gmail.com>
2024-08-06 22:10:28 -0300

    inicio validacao da criacao no front

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/CriarPersonagem.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/CriarPersonagem.controller.js
@@ -1,29 +1,76 @@
-sap.ui.define(["../controller/BaseController"], function (BaseController) {
-  "use strict";
+sap.ui.define(
+  [
+    "../controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "ui5/o_senhor_dos_aneis/services/RacaService",
+  ],
+  function (BaseController, JSONModel, RacaService) {
+    "use strict";
 
-  return BaseController.extend(
-    "ui5.o_senhor_dos_aneis.controller.CriarPersonagem",
-    {
-      //         onInit: function () {
-      //           this.filtros = {};
-      //           const rota = "racas";
-      //           this.vincularRota(rota, this.aoCoincidirRota);
-      //         },
-      //         aoCoincidirRota: function () {
-      //           this.loadRacas();
-      //         },
-      //         loadRacas: async function () {
-      //           this.getRouter().navTo(
-      //             ROTA_RACAS,
-      //             Object.keys(this.filtros).length === LISTA_VAZIA
-      //               ? {}
-      //               : { "?query": this.filtros }
-      //           );
-      //           const racas = await RacaService.obterTodos(this.filtros);
-      //           const modelo = new JSONModel(racas);
-      //           const modeloRaca = "racas";
-      //           this.getView().setModel(modelo, modeloRaca);
-      //         },
-    }
-  );
-});
+    return BaseController.extend(
+      "ui5.o_senhor_dos_aneis.controller.CriarPersonagem",
+      {
+        onInit: function () {
+          this.filtros = {};
+          const rota = "criarPersonagem";
+          this.vincularRota(rota, this.aoCoincidirRota);
+        },
+
+        aoCoincidirRota: function () {
+          this.loadRacas();
+        },
+
+        _verificarCaracteresEspeciais: function (str) {
+          const regex = /[^a-zA-Z0-9]/;
+          return regex.test(str);
+        },
+
+        _verificarTamanhoString: function (str) {
+          let tamanhoMinimo = 3;
+          return str.length < tamanhoMinimo;
+        },
+
+        _validarInputUsuario: function (objetoInput) {
+          let erroValidacao = false;
+          let nomeInseridoInput = objetoInput.getValue();
+
+          let contemCaracteresEspeciais =
+            this._verificarCaracteresEspeciais(nomeInseridoInput);
+
+          if (!contemCaracteresEspeciais) {
+            let valueState = this._verificarTamanhoString(nomeInseridoInput)
+              ? "Error"
+              : "Success"; //valor visual do input do usuario no front
+            objetoInput.setValueState(valueState);
+          }
+
+          //   this._verificarCaracteresEspeciais(textoInput);
+
+          //   try {
+          //     binding.getType().validateValue(inputUsuario.getValue());
+          // 	valueState = "Success";
+          //   } catch (exception) {
+          //     valueState = "Error";
+          //     erroValidacao = true;
+          //   }
+          //   objetoInput.setValueState(valueState);
+
+          return erroValidacao;
+        },
+
+        loadRacas: async function () {
+          const racas = await RacaService.obterTodos(this.filtros);
+          const modelo = new JSONModel(racas);
+          const modeloRaca = "racas";
+
+          this.getView().setModel(modelo, modeloRaca);
+        },
+        aoMudarNome: function (oEvent) {
+          const objetoInput = oEvent.getSource();
+          this._validarInputUsuario(objetoInput);
+          console.log(objetoInput);
+        },
+      }
+    );
+  }
+);
