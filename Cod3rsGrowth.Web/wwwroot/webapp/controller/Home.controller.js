2756cda0915601c85b4c025e2188a422d7cf4dc0
Daniel Souza <danielrsouza10@gmail.com>
2024-08-15 20:52:05 -0300

    fix: navegacao da pagina home para as paginas de listagem por meio do metodo da base controller

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/Home.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/Home.controller.js
@@ -3,13 +3,13 @@ sap.ui.define(["../controller/BaseController"], function (BaseController) {
 
   const ROTA_PERSONAGENS = "listaDePersonagens";
   const ROTA_RACAS = "listaDeRacas";
-  
+
   return BaseController.extend("ui5.o_senhor_dos_aneis.controller.Home", {
     onNavToPersonagens() {
-      this.getRouter().navTo(ROTA_PERSONAGENS);
+      this.onNavTo(ROTA_PERSONAGENS);
     },
     onNavToRacas: function () {
-      this.getRouter().navTo(ROTA_RACAS);
+      this.onNavTo(ROTA_RACAS);
     },
   });
 });
